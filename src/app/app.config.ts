f0abed8f31bff1d1b9fd2caa0e7ad683593084cf
Simonas Rugevicius <simonas.rugevicius@dyflexis.com>
2024-03-25 19:50:31 +0100

    Refactor dice-template code structure and update imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideEffects } from '@ngrx/effects';
-import { DiceEffects } from './dice-template/store/dice.effects';
+import { DiceEffects } from './dice/dice-template/store/dice.effects';
 import { diceReducer } from './dice/dice-template/store/dice.reducers';
 
 export const appConfig: ApplicationConfig = {
--- src/app/dice/dice-template/components/dice-history/dice-history.component.ts
+++ src/app/dice/dice-template/components/dice-history/dice-history.component.ts
@@ -7,15 +7,15 @@ import {
 } from '../../store/dice.reducers';
 import { Store } from '@ngrx/store';
 import { ListType } from '../../models/enums';
-import { DiceHistoryListComponent } from "../dice-history-list/dice-history-list.component";
+import { DiceHistoryListComponent } from '../dice-history-list/dice-history-list.component';
 
 @Component({
-    selector: 'app-dice-history',
-    standalone: true,
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    templateUrl: './dice-history.component.html',
-    styleUrl: './dice-history.component.scss',
-    imports: [CommonModule, DiceHistoryListComponent]
+  selector: 'app-dice-history',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './dice-history.component.html',
+  styleUrl: './dice-history.component.scss',
+  imports: [CommonModule, DiceHistoryListComponent],
 })
 export class DiceHistoryComponent {
   constructor(private store: Store<{ dice: DiceState }>) {}
